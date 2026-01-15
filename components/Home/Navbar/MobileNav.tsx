1fada8a4faa0dc819623b59527d495842a3ec090
kirushikan <kirushikankiru01@gmail.com>
2026-01-15 16:53:22 +0530

    Navbar

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -1,8 +1,51 @@
+"use client"
+import { NavLinks } from '@/constant/constant'
 import React from 'react'
+import Link from 'next/link'
+import { CgClose } from 'react-icons/cg'
 
-const MobileNav = () => {
+type Props = {
+  showNav: boolean
+  closeNav: () => void
+}
+
+const MobileNav = ({ closeNav, showNav }: Props) => {
+  const navOpen = showNav ? "translate-x-0" : "translate-x-full"
+
   return (
-    <div>MobileNav</div>
+    <div className="lg:hidden">
+      
+      {/* Overlay */}
+      <div
+        onClick={closeNav}
+        className={`fixed inset-0 bg-black/70 z-[100002] transition-opacity duration-500
+          ${showNav ? "opacity-100" : "opacity-0 pointer-events-none"}
+        `}
+      ></div>
+
+      {/* Side Nav */}
+      <div
+        className={`fixed top-0 right-0 h-full w-[80%] sm:w-[60%] bg-cyan-800 z-[100050]
+          flex flex-col justify-center space-y-6 text-white
+          transform transition-transform duration-500
+          ${navOpen}
+        `}
+      >
+        {NavLinks.map((link) => (
+          <Link key={link.id} href={link.url} onClick={closeNav}>
+            <p className='w-fit text-xl ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30px]'>
+              {link.label}
+            </p>
+          </Link>
+        ))}
+
+        {/* Close icon */}
+        <CgClose
+          onClick={closeNav}
+          className='absolute top-[0.7rem] right-[1.4rem] w-6 h-6 sm:w-8 sm:h-8 cursor-pointer'
+        />
+      </div>
+    </div>
   )
 }
 
--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -6,24 +6,25 @@ import Link from 'next/link'
 import { BiDownload } from 'react-icons/bi'
 import { HiBars3BottomRight } from 'react-icons/hi2'
 
-const Nav = () => {
+type Props = {
+  openNav: () => void
+}
 
-    const [navBg, setNavBg] =useState(false);
+const Nav = ({ openNav }: Props) => {
+  const [navBg, setNavBg] = useState(false)
 
-    useEffect(()=>{
-        const handler=()=>{
-            if (window.scrollY >=90)setNavBg(true);
-            if (window.scrollY < 90) setNavBg(false);
-        };
-
-        window.addEventListener("scroll",handler);
-
-        return () => window.removeEventListener("scroll",handler);
-    },[])
-
+  useEffect(() => {
+    const handler = () => setNavBg(window.scrollY >= 90)
+    window.addEventListener("scroll", handler)
+    return () => window.removeEventListener("scroll", handler)
+  }, [])
 
   return (
-    <div className={`transition-all ${navBg ? 'bg-[#0f142ed9] shadow-md' : "fixed"} duration-200 h-[12vh] z-[10000] fixed w-full`}>
+    <div
+      className={`fixed w-full h-[12vh] z-[10000] transition-all duration-200
+        ${navBg ? 'bg-[#0f142ed9] shadow-md' : 'bg-transparent'}
+      `}
+    >
       <div className='flex items-center h-full justify-between w-[90%] mx-auto'>
         
         {/* Logo */}
@@ -48,18 +49,19 @@ const Nav = () => {
             </Link>
           ))}
         </div>
-        {/* button*/}
+
+        {/* Buttons */}
         <div className='flex items-center space-x-4'>
-            {/*cv button*/}
-            <button className='px-8 py-3.5 text-sm cursor-pointer rounded-lg bg-blue-800 hover:bg-blue-900 transition-all duration-300 text-white flex items-center space-x-2'>
-                <BiDownload className='w-5 h-5'/>
-                <span>Download CV</span>
-            </button>
-            {/*burger menu*/}
-            <HiBars3BottomRight className='w-8 h-8 cursor-pointer text-white lg:hidden' />
+          <button className='px-8 py-3.5 text-sm rounded-lg bg-blue-800 hover:bg-blue-900 transition-all duration-300 text-white flex items-center space-x-2'>
+            <BiDownload className='w-5 h-5'/>
+            <span>Download CV</span>
+          </button>
 
+          <HiBars3BottomRight
+            onClick={openNav}
+            className='w-8 h-8 cursor-pointer text-white lg:hidden'
+          />
         </div>
-
       </div>
     </div>
   )
