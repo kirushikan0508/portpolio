bcbc8d4a4568db10c16dcbc348848d4c27360d92
kirushikan <kirushikankiru01@gmail.com>
2026-01-15 16:02:34 +0530

    Navbar complete for desktop

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,11 +1,29 @@
+"use client";
 import { NavLinks } from '@/constant/constant'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from 'react-icons/fa'
 import Link from 'next/link'
+import { BiDownload } from 'react-icons/bi'
+import { HiBars3BottomRight } from 'react-icons/hi2'
 
 const Nav = () => {
+
+    const [navBg, setNavBg] =useState(false);
+
+    useEffect(()=>{
+        const handler=()=>{
+            if (window.scrollY >=90)setNavBg(true);
+            if (window.scrollY < 90) setNavBg(false);
+        };
+
+        window.addEventListener("scroll",handler);
+
+        return () => window.removeEventListener("scroll",handler);
+    },[])
+
+
   return (
-    <div className='transition-all duration-200 h-[12vh] z-[10000] fixed w-full'>
+    <div className={`transition-all ${navBg ? 'bg-[#0f142ed9] shadow-md' : "fixed"} duration-200 h-[12vh] z-[10000] fixed w-full`}>
       <div className='flex items-center h-full justify-between w-[90%] mx-auto'>
         
         {/* Logo */}
@@ -29,6 +47,17 @@ const Nav = () => {
               {link.label}
             </Link>
           ))}
+        </div>
+        {/* button*/}
+        <div className='flex items-center space-x-4'>
+            {/*cv button*/}
+            <button className='px-8 py-3.5 text-sm cursor-pointer rounded-lg bg-blue-800 hover:bg-blue-900 transition-all duration-300 text-white flex items-center space-x-2'>
+                <BiDownload className='w-5 h-5'/>
+                <span>Download CV</span>
+            </button>
+            {/*burger menu*/}
+            <HiBars3BottomRight className='w-8 h-8 cursor-pointer text-white lg:hidden' />
+
         </div>
 
       </div>
